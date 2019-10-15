42cfeddc5655ec3066d6f118d5890a53d2d4793a
Caleb Pendergraft <cackmed@gmail.com>
2019-10-14 17:51:48 -0700

    built product array, and added images

---
+++ src/product-list.js
@@ -0,0 +1,166 @@
+const placeholder1 = {
+    id: 'placeHolder1',
+    name: 'Place Holder 1',
+    image: 'place-holder1.jpg',
+};
+const placeholder2 = {
+    id: 'placeHolder2',
+    name: 'Place Holder 2',
+    image: 'place-holder2.jpg',
+};
+const placeholder3 = {
+    id: 'placeHolder3',
+    name: 'Place Holder 3',
+    image: 'place-hoder3.jpg',
+};
+const placeholder4 = {
+    id: 'placeHolder4',
+    name: 'Place Holder 4',
+    image: 'place-holder4.jpg',
+};
+const placeholder5 = {
+    id: 'placeHolder5',
+    name: 'Place Holder 5',
+    image: 'place-holder5.jpg',
+};
+const placeholder6 = {
+    id: 'placeHolder6',
+    name: 'Place Holder 6',
+    image: 'place-holder6.jpg',
+};
+const placeholder7 = {
+    id: 'placeHolder7',
+    name: 'Place Holder 7',
+    image: 'place-holder7.jpg',
+};
+const placeholder8 = {
+    id: 'placeHolder8',
+    name: 'Place Holder 8',
+    image: 'place-holder8.jpg',
+};
+const placeholder9 = {
+    id: 'placeHolder9',
+    name: 'Place Holder 9',
+    image: 'place-holder9.jpg',
+};
+const placeholder10 = {
+    id: 'placeHolder10',
+    name: 'Place Holder 10',
+    image: 'place-holder10.jpg',
+};
+const placeholder11 = {
+    id: 'placeHolder11',
+    name: 'Place Holder 11',
+    image: 'place-holder11.jpg',
+};
+const placeholder12 = {
+    id: 'placeHolder12',
+    name: 'Place Holder 12',
+    image: 'place-holder12.jpg',
+};
+const placeholder13 = {
+    id: 'placeHolder13',
+    name: 'Place Holder 13',
+    image: 'place-holder13.jpg',
+};
+const placeholder14 = {
+    id: 'placeHolder14',
+    name: 'Place Holder 14',
+    image: 'place-holder14.jpg',
+};
+const placeholder15 = {
+    id: 'placeHolder15',
+    name: 'Place Holder 15',
+    image: 'place-holder15.jpg',
+};
+const placeholder16 = {
+    id: 'placeHolder16',
+    name: 'Place Holder 16',
+    image: 'place-holder16.jpg',
+};
+const placeholder17 = {
+    id: 'placeHolder17',
+    name: 'Place Holder 17',
+    image: 'place-holder17.jpg',
+};
+const placeholder18 = {
+    id: 'placeHolder18',
+    name: 'Place Holder 18',
+    image: 'place-holder18.jpg',
+};
+const placeholder19 = {
+    id: 'placeHolder19',
+    name: 'Place Holder 19',
+    image: 'place-holder19.jpg',
+};
+const placeholder20 = {
+    id: 'placeHolder20',
+    name: 'Place Holder 20',
+    image: 'place-holder20.jpg',
+};
+const placeholder21 = {
+    id: 'placeHolder21',
+    name: 'Place Holder 21',
+    image: 'place-holder21.jpg',
+};
+const placeholder22 = {
+    id: 'placeHolder22',
+    name: 'Place Holder 22',
+    image: 'place-holder22.jpg',
+};
+
+const placeholder23 = {
+    id: 'placeHolder23',
+    name: 'Place Holder 23',
+    image: 'place-holder23.jpg',
+};
+const placeholder24 = {
+    id: 'placeHolder24',
+    name: 'Place Holder 24',
+    image: 'place-holder24.jpg',
+};
+const placeholder25 = {
+    id: 'placeHolder25',
+    name: 'Place Holder 25',
+    image: 'place-holder25.jpg',
+};
+const placeholder26 = {
+    id: 'placeHolder26',
+    name: 'Place Holder 26',
+    image: 'place-holder26.jpg',
+};
+const placeholder27 = {
+    id: 'placeHolder27',
+    name: 'Place Holder 27',
+    image: 'place-holder27.jpg',
+};
+const products = [
+    placeholder1,
+    placeholder2,
+    placeholder3,
+    placeholder4,
+    placeholder5,
+    placeholder6,
+    placeholder7,
+    placeholder8,
+    placeholder9,
+    placeholder10,
+    placeholder11,
+    placeholder12,
+    placeholder13,
+    placeholder14,
+    placeholder15,
+    placeholder16,
+    placeholder17,
+    placeholder18,
+    placeholder19,
+    placeholder20,
+    placeholder21,
+    placeholder22,
+    placeholder23,
+    placeholder24,
+    placeholder25,
+    placeholder26,
+    placeholder27,
+];
+export default products;
---
+++ user-stories.md
@@ -0,0 +1,10 @@
+Market Researcher:
+Angela is a senior marketing researcher who wants to be able to be able to easily add new product options that can be tested through a focus group.
+
+
+
+Focus Group Participant:
+Mark is a someone who wishes to take advantage of the focus groups free access to finger sandwiches, and so wants a program that they can quickly push through, so that they can spend more time eating finger sandwiches. 
+
+
+
