6f1a263329aa25f4fb6e0b48ed1c2a82e5642310
Caleb Pendergraft <cackmed@gmail.com>
2019-10-15 11:33:34 -0700

    got dynamic image generation to work

--- src/bus-mall.js
+++ src/bus-mall.js
@@ -1,7 +1,52 @@
-import { products } from './product-list.js';
+import products from './product-list.js';
 import { ProductArray } from './bus-mall-product-class.js';
 
 const radioImage = document.querySelectorAll('img');
 const radioInput = document.querySelectorAll('input');
 const productName = document.getElementById('product-name'); 
 const productArray = new ProductArray(products);
+let quanity = 0;
+
+radioInput.forEach((radioTag) => {
+    radioTag.addEventListener('click', (event) => {
+    });
+});
+
+const productImageIntialization = () => {
+    
+    const randomProduct = productArray.getRandomProduct();
+    let randomProduct2 = productArray.getRandomProduct();
+    let randomProduct3 = productArray.getRandomProduct();
+    console.log(randomProduct);
+    console.log(randomProduct2);
+    console.log(randomProduct3);
+
+    while (randomProduct.id === randomProduct2.id) {
+        randomProduct2 = productArray.getRandomProduct();
+    }
+    while (randomProduct.id === randomProduct3.id || randomProduct3.id === randomProduct2.id) {
+        randomProduct3 = productArray.getRandomProduct();
+    }
+    radioImage.forEach((imageTag, i) => {
+        if (i === 1) {
+            imageTag.src = '../assets/' + randomProduct.image;
+        } else if (i === 0) {
+            imageTag.src = '../assets/' + randomProduct2.image;
+        } else if (i === 2) {
+            imageTag.src = '../assets/' + randomProduct3.image;
+        }
+    });
+    radioInput.forEach((radioTag, i) => {
+        if (i === 1) {
+            radioTag.value = randomProduct.id;
+        } else if (i === 0) {
+            radioTag.calue = randomProduct2.id;
+        } else if (i === 2) {
+            radioTag.value = randomProduct3.id;
+        }
+    });
+};
+
+document.querySelector('button').addEventListener('click', productImageIntialization);
+
+productImageIntialization();
--- src/product-list.js
+++ src/product-list.js
@@ -1,138 +1,138 @@
 const placeholder1 = {
     id: 'placeHolder1',
     name: 'Place Holder 1',
-    image: 'place-holder1.jpg',
+    image: 'placeholder1.png',
 };
 const placeholder2 = {
     id: 'placeHolder2',
     name: 'Place Holder 2',
-    image: 'place-holder2.jpg',
+    image: 'placeholder2.png',
 };
 const placeholder3 = {
     id: 'placeHolder3',
     name: 'Place Holder 3',
-    image: 'place-hoder3.jpg',
+    image: 'placeholder3.png',
 };
 const placeholder4 = {
     id: 'placeHolder4',
     name: 'Place Holder 4',
-    image: 'place-holder4.jpg',
+    image: 'placeholder4.png',
 };
 const placeholder5 = {
     id: 'placeHolder5',
     name: 'Place Holder 5',
-    image: 'place-holder5.jpg',
+    image: 'placeholder5.png',
 };
 const placeholder6 = {
     id: 'placeHolder6',
     name: 'Place Holder 6',
-    image: 'place-holder6.jpg',
+    image: 'placeholder6.png',
 };
 const placeholder7 = {
     id: 'placeHolder7',
     name: 'Place Holder 7',
-    image: 'place-holder7.jpg',
+    image: 'placeholder7.png',
 };
 const placeholder8 = {
     id: 'placeHolder8',
     name: 'Place Holder 8',
-    image: 'place-holder8.jpg',
+    image: 'placeholder8.png',
 };
 const placeholder9 = {
     id: 'placeHolder9',
     name: 'Place Holder 9',
-    image: 'place-holder9.jpg',
+    image: 'placeholder9.png',
 };
 const placeholder10 = {
     id: 'placeHolder10',
     name: 'Place Holder 10',
-    image: 'place-holder10.jpg',
+    image: 'placeholder10.png',
 };
 const placeholder11 = {
     id: 'placeHolder11',
     name: 'Place Holder 11',
-    image: 'place-holder11.jpg',
+    image: 'placeholder11.png',
 };
 const placeholder12 = {
     id: 'placeHolder12',
     name: 'Place Holder 12',
-    image: 'place-holder12.jpg',
+    image: 'placeholder12.png',
 };
 const placeholder13 = {
     id: 'placeHolder13',
     name: 'Place Holder 13',
-    image: 'place-holder13.jpg',
+    image: 'placeholder13.png',
 };
 const placeholder14 = {
     id: 'placeHolder14',
     name: 'Place Holder 14',
-    image: 'place-holder14.jpg',
+    image: 'placeholder14.png',
 };
 const placeholder15 = {
     id: 'placeHolder15',
     name: 'Place Holder 15',
-    image: 'place-holder15.jpg',
+    image: 'placeholder15.png',
 };
 const placeholder16 = {
     id: 'placeHolder16',
     name: 'Place Holder 16',
-    image: 'place-holder16.jpg',
+    image: 'placeholder16.png',
 };
 const placeholder17 = {
     id: 'placeHolder17',
     name: 'Place Holder 17',
-    image: 'place-holder17.jpg',
+    image: 'placeholder17.png',
 };
 const placeholder18 = {
     id: 'placeHolder18',
     name: 'Place Holder 18',
-    image: 'place-holder18.jpg',
+    image: 'placeholder18.png',
 };
 const placeholder19 = {
     id: 'placeHolder19',
     name: 'Place Holder 19',
-    image: 'place-holder19.jpg',
+    image: 'placeholder19.png',
 };
 const placeholder20 = {
     id: 'placeHolder20',
     name: 'Place Holder 20',
-    image: 'place-holder20.jpg',
+    image: 'placeholder20.png',
 };
 const placeholder21 = {
     id: 'placeHolder21',
     name: 'Place Holder 21',
-    image: 'place-holder21.jpg',
+    image: 'placeholder21.png',
 };
 const placeholder22 = {
     id: 'placeHolder22',
     name: 'Place Holder 22',
-    image: 'place-holder22.jpg',
+    image: 'placeholder22.png',
 };
 
 const placeholder23 = {
     id: 'placeHolder23',
     name: 'Place Holder 23',
-    image: 'place-holder23.jpg',
+    image: 'placeholder23.png',
 };
 const placeholder24 = {
     id: 'placeHolder24',
     name: 'Place Holder 24',
-    image: 'place-holder24.jpg',
+    image: 'placeholder24.png',
 };
 const placeholder25 = {
     id: 'placeHolder25',
     name: 'Place Holder 25',
-    image: 'place-holder25.jpg',
+    image: 'placeholder25.png',
 };
 const placeholder26 = {
     id: 'placeHolder26',
     name: 'Place Holder 26',
-    image: 'place-holder26.jpg',
+    image: 'placeholder26.png',
 };
 const placeholder27 = {
     id: 'placeHolder27',
     name: 'Place Holder 27',
-    image: 'place-holder27.jpg',
+    image: 'placeholder27.png',
 };
 const products = [
     placeholder1,
